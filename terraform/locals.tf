82242078de3fba07bf7c303094a5c5b3f27454cd
LarryGF <larrywtf609@gmail.com>
2023-04-10 14:35:27 +0200

    Mostly working services

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,6 @@
 locals {
   k3s_config_file = "~/.kube/config"
   sc_name         = "longhorn"
+  middleware_files = fileset("../helm/traefik/middleware","*.tpl.yaml")
+
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,15 +2,19 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "kubernetes"
+      source  = "kubernetes"
       version = "~> 2.7"
 
     }
     helm = {
-      source = "helm"
-      version = "~> 2.4"
+      source  = "helm"
+      version = "~> 2.9"
     }
- 
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
 # Providers
@@ -19,7 +23,7 @@ provider "kubernetes" {
   experiments {
     manifest_resource = true
   }
-  
+
   config_path = local.k3s_config_file
 }
 
@@ -27,4 +31,10 @@ provider "helm" {
   kubernetes {
     config_path = local.k3s_config_file
   }
+  experiments {
+    manifest = true
+  }
+}
+
+provider "kubectl" {
 }
