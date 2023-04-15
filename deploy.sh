4e1a2472597fd661737f403888aa17ded493f02f
LarryGF <larrywtf609@gmail.com>
2023-04-15 02:08:48 +0200

    -mostly- working refactor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 user=${PI_USER:- pi}
 ansible-playbook ./ansible/k3s-ansible/site.yml -i ./ansible/k3s-ansible/inventory/deploy/hosts.ini -u $user 
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.9/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
-cd terraform && terraform init && terraform apply -auto-approve
+cd terraform && rm -f *.tfstate* && terraform init && terraform apply -auto-approve
