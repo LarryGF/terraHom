16ebab619ba7ba64bec3bf0d5483232613812141
LarryGF <larrywtf609@gmail.com>
2023-05-06 01:33:50 +0200

    Added argocd project

--- terraform/modules/gitops/main.tf
+++ terraform/modules/gitops/main.tf
@@ -9,3 +9,60 @@ resource "argocd_repository" "default-repo" {
   name = "pi-k8s"
   type = "git"
 }
+resource "argocd_project" "gitops" {
+  metadata {
+    name      = "gitops"
+    namespace = "gitops"
+    labels = {
+      acceptance = "true"
+    }
+  }
+
+  spec {
+    description = "Project to deploy all internal infrastructure"
+
+    source_namespaces = ["gitops"]
+    source_repos      = ["*"]
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "services"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "monitoring"
+    }
+
+    cluster_resource_blacklist {
+      group = "*"
+      kind  = "*"
+    }
+    cluster_resource_whitelist {
+      group = "rbac.authorization.k8s.io"
+      kind  = "ClusterRoleBinding"
+    }
+    cluster_resource_whitelist {
+      group = "rbac.authorization.k8s.io"
+      kind  = "ClusterRole"
+    }
+    
+    namespace_resource_whitelist {
+      group = "*"
+      kind  = "*"
+    }
+
+ role {
+      name = "gitopsAdmin"
+      policies = [
+        "p, proj:gitops:gitopsAdmin, applications, override, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, applications, sync, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, clusters, get, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, repositories, create, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, repositories, delete, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, repositories, update, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, logs, get, gitops/*, allow",
+        "p, proj:gitops:gitopsAdmin, exec, create, gitops/*, allow",
+      ]
+    }
+  }
+}
