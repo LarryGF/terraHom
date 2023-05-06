e45036aa08ca188a5ae55b79e152ba6037b74d44
LarryGF <larrywtf609@gmail.com>
2023-05-06 21:10:17 +0200

    Not publishing unhealthy services

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,6 @@ printf "Running ansible playbook\n"
 printf "You will be running this as ${PI_USER}\nType your password below:\n"
 ansible-playbook ./ansible/site.yml -i ./ansible/inventory/deploy/hosts.ini --ask-become-pass --user $user
 printf "Ansible playbook complete\n"
-# printf "Scaling down kube-system pods\n"
-# . ./scripts/scale_down.sh "-n kube-system" 0
-
-printf "Scaling up  kube-system pods services and waiting for completion\n"
-
-printf "CoreDNS\n"
-kubectl -n kube-system scale deployment coredns --replicas=1
-kubectl wait -n kube-system deployment/coredns --for=condition=available --timeout=5m
-kubectl wait -n kube-system pods -l k8s-app="kube-dns" --for=condition=ready --timeout=5m
-printf "Local Path Provisioner\n"
-kubectl -n kube-system scale deployment local-path-provisioner --replicas=1
-kubectl wait -n kube-system deployment/local-path-provisioner --for=condition=available --timeout=5m
-kubectl wait -n kube-system pods -l app="local-path-provisioner" --for=condition=ready --timeout=5m
-printf "Metrics Server\n"
-kubectl -n kube-system scale deployment metrics-server --replicas=1
-kubectl wait -n kube-system deployment/metrics-server --for=condition=available --timeout=5m
-kubectl wait -n kube-system pods -l k8s-app=metrics-server --for=condition=ready --timeout=5m
 
 printf "Installing CRDs\n"
 # kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.crds.yaml
