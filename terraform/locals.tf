7776a9109cc9ea69c3d487c610fd81554393f951
LarryGF <larrywtf609@gmail.com>
2023-05-06 23:13:39 +0200

    Trying to add duplicati

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,25 +3,44 @@ locals {
 
   sc_name = var.use_longhorn ? "longhorn" : "local-path"
 
-  applications = yamldecode(templatefile("applications.yaml",
+  template_applications = yamldecode(templatefile("applications.yaml",
     {
       duckdns_domain   = var.duckdns_domain
       master_hostname  = var.master_hostname
       allowed_networks = var.allowed_networks
-      volume_mounts = local.duplicati_mounts
 
     }
   ))
 
-  duplicati_mounts = { for app in local.applications :
-     app.name => {
-      enabled = true,
-      mountPath = "/config/${app.name}",
+  applications = merge(local.template_applications, local.duplicati_definition)
+
+  duplicati_mounts = { for app in local.template_applications :
+    app.name => {
+      enabled       = true,
+      mountPath     = "/config/${app.name}",
       existingClaim = app.volumes.config.name
-    } if app.deploy && app.namespace == "services" && contains(keys(app.volumes),"config")
+    } if app.deploy && app.namespace == "services" && contains(keys(app.volumes), "config")
+  }
+
+  vscode_mounts = [
+    for key, value in local.duplicati_mounts : { "name" : key, "mountPath" : value["mountPath"] }
+  ]
+
+  duplicati_definition = {
+    "duplicati" : {
+      "name" : "duplicati",
+      "namespace" : "services",
+      "deploy" : true,
+      "volumes" : {}
+      "override" : {
+        "duckdns_domain" : var.duckdns_domain,
+        "volume_mounts" : indent(4,yamlencode(local.duplicati_mounts))
+        "vscode_volume_mounts" : indent(8, yamlencode(local.vscode_mounts))
+      }
+    }
   }
 }
 
 output "duplicati" {
-  value = local.duplicati_mounts
+  value = local.duplicati_definition
 }
