bb60442ee00beb76cdf55747e8d66c80eacaec6d
LarryGF <larrywtf609@gmail.com>
2023-05-06 22:23:42 +0200

    edited minimal config

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -10,4 +10,16 @@ locals {
       allowed_networks = var.allowed_networks
     }
   ))
+
+  duplicati_mounts = { for app in local.applications :
+    app.deploy && app.namespace == "services" && app.volumes.config.name => {
+      enabled = true,
+      mountPath = "/config/${replace(app.volumes.config.name, "-config", "")}",
+      existingClaim = app.volumes.config.name
+    }
+  }
+}
+
+output "duplicati" {
+  value = local.duplicati_mounts
 }
