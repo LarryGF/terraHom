79ad151beafdb2fbef86f6a6f33695477f7d9b56
LarryGF <larrywtf609@gmail.com>
2023-05-07 02:45:55 +0200

    Removed deprecated folder, still stored in  branch

--- deprecated/rancher/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "letsencrypt_email" {
-  type        = string
-  description = "Email to use for Let's Encrypt certificates"
-}
-
-variable "duckdns_domain" {
-  type        = string
-  description = "DuckDNS domain to use"
-}
