6261d83a5412870b81e9feb39a9c5c36e9643d4d
LarryGF <larrywtf609@gmail.com>
2023-06-18 20:50:17 +0200

    Moved prometheus crds outside prometheus chart

---
+++ terraform/modules/base/submodules/prometheus-crds/main.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "duckdns" {
+  name         = "prometheus-operator-crds"
+  chart        = "prometheus-operator-crds"
+  repository   = "https://prometheus-community.github.io/helm-charts"
+  namespace    = "monitoring"
+  reuse_values = true
+  cleanup_on_fail = true
+  wait            = true
+  wait_for_jobs   = true
+  timeout         = 180
+  version= 4.0.2
+}
