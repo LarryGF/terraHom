788a62cc1398bcbd8ec079e89d5f55270b91d9d4
LarryGF <larrywtf609@gmail.com>
2023-08-14 21:18:11 +0200

    Trying to use mailhog with authelia

---
+++ scripts/create_authelia_users.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Prompt for username, email, and password
+read -p "Enter username: " username
+read -p "Enter email: " email
+read -s -p "Enter password: " password
+echo
+
+# Hash the password using Argon2
+hashed_password=$(echo -n "$password" | argon2 eUhVT1dQa082YVk2VUhDMQ -id -t 1 -m 15 -p 8 -l 32 | awk -F ': ' '/Encoded/{print $2}')
+
+# Generate the required YAML format and append to users.config
+cat <<EOL >> ./users.config
+${username}:
+  disabled: false
+  displayname: ${username}
+  password: ${hashed_password}
+  email: ${email}
+  groups:
+    - admins
+    - dev
+
+EOL
+
+echo "User details appended to users.config"
