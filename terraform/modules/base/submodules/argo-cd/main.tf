172d25aae7c163f0e9ea371fc421687b68f70264
LarryGF <larrywtf609@gmail.com>
2024-03-30 19:52:01 +0100

    chore: bumping argo release

--- terraform/modules/base/submodules/argo-cd/main.tf
+++ terraform/modules/base/submodules/argo-cd/main.tf
@@ -5,7 +5,7 @@ resource "helm_release" "argo-cd" {
   repository = "https://argoproj.github.io/argo-helm"
   namespace  = "gitops"
   reuse_values = false
-  version = "5.53.2"
+  version = "6.7.6"
   set {
     name  = "env.TZ"
     value = var.timezone
